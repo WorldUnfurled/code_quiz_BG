2fd32728a6f126b2ee74930f3acfee985c2866c5
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-06-22 17:49:02 -0400

    Created lists for questions and answers; Created displayQuestions function that prints answers from the selected answer array and prints them onto the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,34 @@
 var time = document.querySelector('#time');
+var answerList = document.querySelector('#question-list');
 
 var timeLeft = 60;
+
+var questions = [
+    "What tag is used to link a Javascript file to the HTML?",
+    "The programming interface that represents a webpage with text is known as ____", 
+    "Typing console.log(this) in the global scope displays ____ to the console"
+];
+
+firstAnswers = [
+    '<js>',
+    '<script>',
+    '<java>',
+    '<jquery>'
+];
+
+secondAnswers = [
+    'React',
+    'jQuery',
+    'DOM',
+    'JSON'
+];
+
+thirdAnswers = [
+    'Window',
+    'Document',
+    'Body',
+    'Main'
+];
 
 function playTime() {
     // Sets interval in variable
@@ -15,4 +43,15 @@ function playTime() {
     }, 1000);
 }
 
+function displayFirstQuestions () {
+
+
+    for (var i = 0; i < firstAnswers.length; i++) {
+        answerList.children[i].children[0].textContent = i + ". " + firstAnswers[i];
+        // console.log(answerList.children.eq(i).firstChild.textContent);
+        console.log(answerList.children[i].children[0].textContent);
+    }
+}
+
 playTime();
+displayFirstQuestions();
